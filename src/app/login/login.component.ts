5b4755b227208e836a0af0f159aab0d3ff5f9a8d
mahamfathy <mahafathyh@gmail.com>
2024-07-31 18:28:49 +0300

    adding the login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass'
+  styleUrl: './login.component.sass',
 })
 export class LoginComponent {
-
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
