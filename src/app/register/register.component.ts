3c4b863c52dad7d47bde9d291481da1d8791de8d
mahamfathy <mahafathyh@gmail.com>
2024-08-02 21:43:58 +0300

    put some validation for register comp

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -21,25 +26,41 @@ import { SharedModule } from '../shared/shared.module';
   templateUrl: './register.component.html',
   styleUrl: './register.component.sass',
 })
-export class RegisterComponent {
-  form: FormGroup;
+export class RegisterComponent implements OnInit {
+  private title!: string;
+  form!: FormGroup;
   userName = new FormControl<string>('test', {
     updateOn: 'blur',
     nonNullable: false,
+    validators: [Validators.required, Validators.minLength(8)],
   });
-  constructor() {
-    this.form = this.getFormModels();
+  constructor(public formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.initFormModels();
   }
-  private getFormModels(): FormGroup {
-    return new FormGroup({
+  private initFormModels(): void {
+    // this.form = this.formBuilder.group({
+    //   userName: ['', Validators.required],
+    //   email: ['', [Validators.required, Validators.email]],
+    //   password: ['', [Validators.required, Validators.minLength(8)]],
+    //   confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    //   mobile: ['', Validators.required],
+    // });
+    this.form = new FormGroup({
       userName: this.userName,
       email: new FormControl<string>({ value: '', disabled: false }),
-      password: new FormControl(''),
-      mobile: new FormControl(''),
-      confirmPassword: new FormControl(''),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(8)],
+      }),
+
+      mobile: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(8)],
+      }),
     });
   }
   reset(): void {
-    this.userName.reset({ value: 'maha', disabled: false });
+    this.title = 'maha';
+    this.userName.reset({ value: this.title, disabled: false });
   }
 }
