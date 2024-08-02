d026c794f20fd21ce081502ddc7f51b4af72faa4
mahamfathy <mahafathyh@gmail.com>
2024-08-02 17:48:26 +0300

    init formgroup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SharedModule } from '../shared/shared.module';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    SharedModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.sass'
+  styleUrl: './register.component.sass',
 })
 export class RegisterComponent {
-
+  form: FormGroup;
+  userName = new FormControl<string>('test', {
+    updateOn: 'blur',
+    nonNullable: false,
+  });
+  constructor() {
+    this.form = this.getFormModels();
+  }
+  private getFormModels(): FormGroup {
+    return new FormGroup({
+      userName: this.userName,
+      email: new FormControl<string>({ value: '', disabled: false }),
+      password: new FormControl(''),
+      mobile: new FormControl(''),
+      confirmPassword: new FormControl(''),
+    });
+  }
+  reset(): void {
+    this.userName.reset({ value: 'maha', disabled: false });
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [],
-  imports: [TranslateModule, CommonModule, FormsModule],
-  exports: [TranslateModule, CommonModule, FormsModule],
+  imports: [TranslateModule, CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [TranslateModule, CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
